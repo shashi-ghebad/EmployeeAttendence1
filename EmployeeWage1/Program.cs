69502daa11b604133a07387b920985280224bac8
Shashi <shashihghebad3@gmail.com>
2021-03-03 05:20:18 -0800

    [SHASHI]Add:UC6-Monthlywage20Days

--- EmployeeWage1/Program.cs
+++ EmployeeWage1/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace UC5_MonthlyWage
+namespace UC6_MonthlyWage20Days
 {
     class Program
     {
@@ -10,21 +10,21 @@ namespace UC5_MonthlyWage
             int Emp_Rate = 41;
             int empHrs = 8;
             int empWage = 0;
-            int count = 0;
+            int Total = 20;
             Random random = new Random();
             for (int i = 1; i <= 41; i++)
             {
-                int empCheck = random.Next(0, 2);
+                int empCheck = random.Next(1);
                 if (empCheck == PRESENT)
                 {
-                    count++;
+                    Total++;
 
                 }
 
             }
-            empWage = count * empHrs * Emp_Rate;
+            empWage = Total * empHrs * Emp_Rate;
 
-            Console.WriteLine("Employee Wages For " + count + " Working Days " + empWage);
+            Console.WriteLine("Employee Wages For " + Total + " Working Days " + empWage);
         }
     }
 }
