1a1cfa4af96eaabf9c3e1f2b545b797f6cb85e01
Shashi <shashihghebad3@gmail.com>
2021-03-06 09:40:08 -0800

    [SHASHI]Add:UC7-EmpWageUsingClassMethod

--- EmployeeWage1/Program.cs
+++ EmployeeWage1/Program.cs
@@ -1,30 +1,48 @@
 ﻿using System;
 
-namespace UC6_MonthlyWage20Days
-{
+namespace UC7_EmpWageUsingClassMethod
+{ 
     class Program
+{
+    public const int IS_PART_TIME = 1;
+    public const int IS_FULL_TIME = 2;
+    public const int EMP_RATE_PER_HOUR = 20;
+    public const int NUM_OF_WORKING_DAYS = 2;
+    public const int MAX_HRS_IN_MONTH = 10;
+    public static int computeEmpWage()
+
     {
-        public static void Main(string[] args)
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS )
         {
-            int PRESENT = 1;
-            int Emp_Rate = 41;
-            int empHrs = 8;
-            int empWage = 0;
-            int Total = 20;
-            Random random = new Random();
-            for (int i = 1; i <= 41; i++)
-            {
-                int empCheck = random.Next(1);
-                if (empCheck == PRESENT)
-                {
-                    Total++;
+        totalWorkingDays++;
+        Random random = new Random();
+        int empCheck = random.Next(0, 3);
+        switch (empCheck)
+        {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
 
-                }
-
-            }
-            empWage = Total * empHrs * Emp_Rate;
-
-            Console.WriteLine("Employee Wages For " + Total + " Working Days " + empWage);
         }
+        totalEmpHrs += empHrs;
+        Console.WriteLine("Day#:" + totalWorkingDays + "EmpHrs : " + empHrs );
+    
+    }
+    int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+    Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+    return totalEmpWage;
+    }
+    static void Main(string[] args)
+    {
+    computeEmpWage();
+
+    }
     }
 }
